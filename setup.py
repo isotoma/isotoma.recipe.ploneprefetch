8b8401c7cae58d7942a148963a3c18381a2cc3a8
John Carr <john.carr@isotoma.com>
2012-10-31 16:20:33 +0000

    Back to development: 0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="isotoma.recipe.ploneprefetch",
-    version='0.0.6',
+    version='0.0.7.dev0',
     maintainer = "John Carr",
     maintainer_email = "john.carr@isotoma.com",
     description="ZC Buildout recipe for installing Plone quickly",
